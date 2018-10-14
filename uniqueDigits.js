90ba3185953c299149bb99b0d4f89863a3e63f85
Mahesh Sovani <44019471+maheshsovani@users.noreply.github.com>
2018-10-14 12:53:35 +0530

    One more file is added
    This file filters the unique elements of the array.
    and returns the array of unique elements.

---
+++ uniqueDigits.js
@@ -0,0 +1,15 @@
+const filterUniqueElements=function(array){
+  let uniqueArray=[];
+  for (element of array){
+    if(!uniqueArray.includes(element)){
+      uniqueArray.push(element);
+    }
+  }
+  return uniqueArray;
+}
+const main=function(){
+  array=[1,2,3,4,5,6,4,3,5,2];
+  console.log(filterUniqueElements(array))
+}
+main();
+exports.filterUniqueElements=filterUniqueElements;
